e6c7c71cb32ce8f2454505c3c7e659c8430cbdc3
Daniel dos Santos <154810817+danieldelimadev@users.noreply.github.com>
2024-01-25 13:38:25 -0300

    adicionei ex14 ex15

---
+++ Exercicios/ex014.py
@@ -0,0 +1,3 @@
+c = int(input('Informe a temperatura em °C: '))
+f = (c * 9 / 5) + 32 
+print('A temperatura em fahrenheit é: {:.0f}°F'.format(f))
---
+++ Exercicios/ex015.py
@@ -0,0 +1,4 @@
+d = int(input('Quantos dias alugados? '))
+k = int(input('Quantos Km rodados? '))
+pdia = d * 60
+pkm = k  * 0.15
