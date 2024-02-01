4b34a9773b70c06b53462968c082a2ef5c172243
Daniel dos Santos <154810817+danieldelimadev@users.noreply.github.com>
2024-01-31 21:18:23 -0300

    att o des 072

--- Desafios/des072.py
+++ Desafios/des072.py
@@ -1,10 +1,21 @@
+from os import system
+system('cls')
 tupla = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete',  'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezeseis', 'dezesete', 'dezoito', 'dezenove', 'vinte')
-n = int(input('Digite um número entre 0 e 20: '))
-if 0 <= n <= 20:
-    print(f'Você digitou o número \033[1m{tupla[n]}\033[m')
-else:
-    while True:
-        n = int(input('Tente novamente. Digite um número entre 0 e 20: '))
-        if n >= 0 and n <= 20: 
-            print(f'Você digitou o número \033[1m{tupla[n]}\033[m')
+cont = 0
+while True:
+    n = int(input('Digite um número: '))
+    if  0 <= n <= 20:
+        print(f'Você digitou o número \033[1m{tupla[n]}\033[m')
+        while True:
+            res = input('Quer ver outro número? \033[1mS/N\033[m ')
+            if res in 'SsNn':
+                break
+        if res in 'Nn':
             break
+        cont += 1
+        if cont % 4 == 0:
+            system('cls')
+    else:
+        print('Tente novamente.', end=' ')
+    
+        
